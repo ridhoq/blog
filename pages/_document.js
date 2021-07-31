b457a2b4e42b337b2e8647fccdc9e2b0c89e812d
Ridwan Hoq <943533+ridhoq@users.noreply.github.com>
2021-07-31 18:11:01 +0000

    first pass

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'ridhoq.dev',
+      description: 'Ridwan Hoq is a software developer',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
